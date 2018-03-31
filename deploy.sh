f6e3f74ca7b94aa173d069509ff528e0c26badb2
Sachin Shetty <sshetty@egnyte.com>
2018-03-31 12:28:50 +0530

    use generic

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/bash -xe
 echo $K8S_SERVICE_ACCOUNT_KEY | base64 --decode -i > ${HOME}/k8s-service-key.json
 gcloud auth activate-service-account --key-file ${HOME}/k8s-service-key.json
 gcloud container --project free-apis-199609  clusters get-credentials  free-apis-infra  --zone asia-south1-a
