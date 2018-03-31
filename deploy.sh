79e044bedaa7297dbcd5bad92bd96f4ad9abbeb2
Sachin Shetty <sshetty@egnyte.com>
2018-03-31 12:07:36 +0530

    fix travis.yml

---
+++ deploy.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+echo $K8S_SERVICE_ACCOUNT_KEY | base64 --decode -i > ${HOME}/k8s-service-key.json
+gcloud auth activate-service-account --key-file ${HOME}/k8s-service-key.json
+gcloud container --project free-apis-199609  clusters get-credentials  free-apis-infra  --zone asia-south1-a
+helm upgrade --install geo-ip free-apis-helm-chart/ --values free-apis-helm-chart/values.yaml --namespace=default --debug
+rm ${HOME}/k8s-service-key.json
